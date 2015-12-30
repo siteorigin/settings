b9372e21f30b58f00ecbe54bb96f876983d73149
gregpriday <greg@siteorigin.com>
2015-12-30 18:37:19 +0200

    Added script to fetch Google webfonts

---
+++ bin/fetch_google_fonts.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php -q
+<?php
+
+$key = '';
+$response = file_get_contents( 'https://www.googleapis.com/webfonts/v1/webfonts?key=' . urlencode( $key ) );
+
+$fonts = json_decode( $response, true )['items'];
+$return = array();
+
+foreach( $fonts as $font ) {
+	$family = $font['family'];
+	foreach( array_keys($font) as $key ) {
+		if( !in_array( $key, array('variants', 'subsets', 'category') ) )  {
+			unset( $font[$key] );
+		}
+	}
+
+	$return[$family] = $font;
+}
+
+// Sort the keys, just incase
+ksort( $return );
+
+echo 'Writing fonts to: ' . realpath( dirname(__FILE__). '/../inc/fonts.php' ) . "\n";
+$contents = "<?php\n\n";
+$contents .= 'return ' . str_replace('  ', "\t", var_export( $return, true ) ) . ';';
+file_put_contents( realpath( dirname(__FILE__). '/../inc/fonts.php' ), $contents );
