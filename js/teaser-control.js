a4c8cfcbdbd28fd1be061b21f30df1233b0ad8f5
gregpriday <greg@siteorigin.com>
2015-12-30 17:03:06 +0200

    Removed modal

--- js/teaser-control.js
+++ js/teaser-control.js
@@ -13,24 +13,7 @@ jQuery( function($){
             var container = control.container;
 
             container.find('.so-premium-upgrade').click( function( e ){
-                e.preventDefault();
-
-                // Setup the modal
-                if( typeof premiumModal === 'undefined' ) {
-                    premiumModal = $( $('#so-premium-modal-template').html().trim() ).hide().appendTo( 'body' ).fadeIn( 'fast' );
-
-                    // Setup the premium modal
-                    premiumModal.find( '.so-modal-close').click( function(){
-                        premiumModal.fadeOut('fast');
-                    } );
-
-                    $.get( ajaxurl + "?action=so_settings_premium_content", function( r ){
-                        console.log( r );
-                    } );
-                }
-                else {
-                    premiumModal.fadeIn('fast');
-                }
+                // We can show a modal here at some point, for now we just direct users to SiteOrigin
             } );
         }
     } );
