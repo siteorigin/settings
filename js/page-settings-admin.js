fa501d166d85e20b029583eeb13f740b09d6a2d1
gregpriday <greg@siteorigin.com>
2016-05-06 14:53:39 +0200

    Rehide unused sections on pane reflow

--- js/page-settings-admin.js
+++ js/page-settings-admin.js
@@ -1,14 +1,30 @@
 ( function( $ ){
 	var api = wp.customize;
 
+	var hideSections = false;
+	var showSections = false;
+	api.bind( 'pane-contents-reflowed', function(){
+		if( hideSections ) {
+			hideSections.hide();
+		}
+		if( showSections ) {
+			showSections.show();
+		}
+	} );
+
 	$(function(){
 		api.previewer.bind( 'page-settings', function( message ) {
 			// accordion-section-page_settings_template_home
 			var $section = $( '#accordion-section-page_settings_' + message[0] + '_' + message[1] );
-			var $all_sections = $('[id^="accordion-section-page_settings_"]');
+			var $all_sections = $('[id^="accordion-section-page_settings_"]' );
 
-			$all_sections.not( $section ).hide();
-			$section.show();
+			if( $section.length === 0 ) {
+				$all_sections.show();
+			}
+			else {
+				hideSections = $all_sections.not( $section ).hide();
+				showSections = $section.show();
+			}
 		} );
 	});
 } )( jQuery );
