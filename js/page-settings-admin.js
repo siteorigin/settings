a0a3f68abb3e30732262b7b3fb196ab1393fff7a
gregpriday <greg@siteorigin.com>
2016-05-06 14:32:43 +0200

    Removed console.log

--- js/page-settings-admin.js
+++ js/page-settings-admin.js
@@ -7,9 +7,6 @@
 			var $section = $( '#accordion-section-page_settings_' + message[0] + '_' + message[1] );
 			var $all_sections = $('[id^="accordion-section-page_settings_"]');
 
-			console.log( $section.length );
-			console.log( $all_sections.length );
-
 			$all_sections.not( $section ).hide();
 			$section.show();
 		} );
