ecf854f1e9af1ca6b9b8ccd29a61c9b351b1e330
gregpriday <greg@siteorigin.com>
2016-05-31 09:51:43 +0200

    Support for contributor affiliates program

--- settings.php
+++ settings.php
@@ -40,6 +40,7 @@ class SiteOrigin_Settings {
 		}
 
 		add_action( 'after_setup_theme', array( $this, 'load_settings_extras' ) );
+		add_filter( 'siteorigin_premium_theme_affiliate_id', array( $this, 'contributor_affiliate_id' ) );
 
 		spl_autoload_register( array( $this, '_autoload' ) );
 	}
@@ -774,6 +775,33 @@ class SiteOrigin_Settings {
 		$url = add_query_arg( $args, 'https://siteorigin.com/downloads/premium/' );
 
 		return $url;
+	}
+
+	/**
+	 * Add a random affiliate ID based on contributors file
+	 */
+	function contributor_affiliate_id( $aff ){
+		if( file_exists( get_template_directory() . '/inc/contributors.php' ) ) {
+			static $contributor = false;
+			if( empty( $contributor ) ) {
+				$contributors = include get_template_directory() . '/inc/contributors.php';
+				shuffle( $contributors );
+
+				$rand = rand( 0, 10000 ) / 100;
+				$sum = 0;
+
+				foreach ( $contributors as $c ) {
+					$sum += floatval( $c['percent'] );
+					if ( $sum >= $rand ) {
+						$contributor = $c[ 'email' ];
+						break;
+					}
+				}
+			}
+			$aff = $contributor;
+		}
+
+		return $aff;
 	}
 }
 
