7f190c222540da1fb12e507096cb470e1cf0dfac
Greg Priday <greg@siteorigin.com>
2016-08-19 11:35:03 +0200

    Class comment

--- inc/upgrade.php
+++ inc/upgrade.php
@@ -1,6 +1,10 @@
 <?php
 
-
+/**
+ * A legacy upgrade notice that we'll temporarily include for users on Vantage free who have an order number saved.
+ *
+ * Class SiteOrigin_Settings_Upgrade
+ */
 class SiteOrigin_Settings_Upgrade {
 
 	function __construct(){
