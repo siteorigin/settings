5b1ccf77c7c0bd6dcee8beb8df3dace8f86939d9
gregpriday <greg@siteorigin.com>
2016-10-06 11:24:16 +0200

    Reset to Page Settings section after changing URL
    Resolves GH-16

--- js/page-settings-admin.js
+++ js/page-settings-admin.js
@@ -15,8 +15,14 @@
 	$(function(){
 		api.previewer.bind( 'page-settings', function( message ) {
 			// accordion-section-page_settings_template_home
-			var $section = $( '#accordion-section-page_settings_' + message[0] + '_' + message[1] );
-			var $all_sections = $('[id^="accordion-section-page_settings_"]' );
+			var $section = $( '#accordion-section-page_settings_' + message[0] + '_' + message[1] ),
+				$all_sections = $( '[id^="accordion-section-page_settings_"]' ),
+				$open = $( '[id^="accordion-section-page_settings_"].open' );
+
+			if( $open.length ) {
+				// We'll return to the Page Settings section
+				$( '#accordion-panel-page_settings .accordion-section-title' ).trigger( 'click' );
+			}
 
 			hideSections = $all_sections.not( $section ).hide();
 			showSections = $section.show();
