57425c1388d2b0ed0b0bc57ab0b45f79c312b926
gregpriday <greg@siteorigin.com>
2017-01-20 08:59:32 +0200

    Do once off setting migrations

--- settings.php
+++ settings.php
@@ -40,6 +40,7 @@ class SiteOrigin_Settings {
 		}
 
 		add_action( 'after_setup_theme', array( $this, 'load_settings_extras' ) );
+		add_action( 'after_setup_theme', array( $this, 'handle_migrations' ) );
 
 		spl_autoload_register( array( $this, '_autoload' ) );
 
@@ -95,24 +96,29 @@ class SiteOrigin_Settings {
 		}
 
 		// Handle setting migration
-		$value =  get_theme_mod( 'theme_settings_' . $setting, $default );
-		static $migrated_settings;
-		if( is_null( $migrated_settings ) ) {
-			// Migrated settings are used to split settings or change their keys
-			$migrated_settings = apply_filters( 'siteorigin_settings_migrated_settings', array(  ) );
-		}
-		if( ! empty( $migrated_settings[ $setting ] ) ) {
-			// This setting is migrating from somewhere else
-			$raw_value = get_theme_mod( 'theme_settings_' . $setting, null );
-			if( is_null( $raw_value ) ) {
-				$value = get_theme_mod( 'theme_settings_' . $migrated_settings[ $setting ], $default );
+		return apply_filters( 'siteorigin_setting', get_theme_mod( 'theme_settings_' . $setting, $default ), $setting );
+	}
+
+	/**
+	 * Handle migration of settings from one key to another
+	 */
+	function handle_migrations(){
+		$migrations = apply_filters( 'siteorigin_settings_migrated_settings', array(  ) );
+		if( empty( $migrations ) ) return;
+
+		$migration_key = md5( serialize( $migrations ) );
+		if( $migration_key !== get_theme_mod( 'migration_key' ) ) {
+			foreach( $migrations as $to => $from ) {
+				$raw_to = get_theme_mod( 'theme_settings_' . $to, null );
+				$raw_from = get_theme_mod( 'theme_settings_' . $from, null );
+
+				if( is_null( $raw_to ) && ! is_null( $raw_from ) ) {
+					set_theme_mod( 'theme_settings_' . $to, $raw_from );
+				}
 			}
+
+			set_theme_mod( 'migration_key', $migration_key );
 		}
-
-		// Return a filtered version of the setting
-		$value = apply_filters( 'siteorigin_setting', $value, $setting );
-
-		return $value;
 	}
 
 	/**
