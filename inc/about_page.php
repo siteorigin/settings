50a90b47dd7dd0c73ccc3df7cb5dd6de9e0c35bf
Greg Priday <greg@siteorigin.com>
2017-12-31 00:19:13 +0200

    Added the no_video flag

--- inc/about_page.php
+++ inc/about_page.php
@@ -209,12 +209,14 @@ class SiteOrigin_Settings_About_Page {
 				<div class="about-video">
 					<div class="about-container">
 						<a href="<?php echo esc_url( $about[ 'video_url' ] ) ?>" class="about-play-video" target="_blank">
-							<svg version="1.1" id="play" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-							     viewBox="0 0 540 320.6" style="enable-background:new 0 0 540 320.6;" xml:space="preserve">
-								<path class="st0" d="M511,0H29C13,0,0,13,0,29v262.6c0,16,13,29,29,29h482c16,0,29-13,29-29V29C540,13,527,0,511,0z"/>
-								<path class="st1" d="M326.9,147.3c4.2,2.6,6.9,7.6,6.9,13c0,5.4-2.7,10.3-7.2,13.2l-94.9,69.9c-2.6,2.2-6.1,3.5-9.8,3.5
-								c-8.7,0-15.7-7-15.7-15.7V89.4c0-8.6,7-15.7,15.7-15.7c3.7,0,7.3,1.3,10.1,3.7L326.9,147.3z"/>
-							</svg>
+							<?php if( empty( $about[ 'no_video' ] ) ) : ?>
+								<svg version="1.1" id="play" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								     viewBox="0 0 540 320.6" style="enable-background:new 0 0 540 320.6;" xml:space="preserve">
+									<path class="st0" d="M511,0H29C13,0,0,13,0,29v262.6c0,16,13,29,29,29h482c16,0,29-13,29-29V29C540,13,527,0,511,0z"/>
+									<path class="st1" d="M326.9,147.3c4.2,2.6,6.9,7.6,6.9,13c0,5.4-2.7,10.3-7.2,13.2l-94.9,69.9c-2.6,2.2-6.1,3.5-9.8,3.5
+									c-8.7,0-15.7-7-15.7-15.7V89.4c0-8.6,7-15.7,15.7-15.7c3.7,0,7.3,1.3,10.1,3.7L326.9,147.3z"/>
+								</svg>
+							<?php endif ?>
 						</a>
 
 						<div class="about-video-images">
@@ -232,11 +234,13 @@ class SiteOrigin_Settings_About_Page {
 							?>
 						</div>
 
-						<div class="about-video-watch">
-							<a href="<?php echo esc_url( $about[ 'video_url' ] ) ?>" target="_blank">
-								<?php esc_html_e( 'Watch The Video', 'siteorigin' ) ?>
-							</a>
-						</div>
+						<?php if( empty( $about[ 'no_video' ] ) ) : ?>
+							<div class="about-video-watch">
+								<a href="<?php echo esc_url( $about[ 'video_url' ] ) ?>" target="_blank">
+									<?php esc_html_e( 'Watch The Video', 'siteorigin' ) ?>
+								</a>
+							</div>
+						<?php endif ?>
 
 						<?php if( ! empty( $about['description'] ) ) : ?>
 							<div class="about-video-description">
