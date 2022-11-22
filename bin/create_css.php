fd6472366e5346721dba1521a043a38dd9b0b4a4
Alex S <17275120+AlexGStapleton@users.noreply.github.com>
2022-11-22 15:19:14 +1300

    Formatting

--- bin/create_css.php
+++ bin/create_css.php
@@ -5,7 +5,7 @@ include dirname( __FILE__ ) . '/inc/recursive_copy.php';
 
 // PHP 8 Compatibility function required by cssmin.
 // https://www.php.net/manual/en/function.each.php#126076
-function each( $array ){
+function each( $array  ){
 	$key = key( $array );
 	$value = current( $array );
 	$each = is_null( $key ) ? false : array(
