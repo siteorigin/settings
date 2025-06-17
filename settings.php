e568ad3bf9a012c3d822df00d11122d6e5525f1b
Andrew Misplon <amisplon@gmail.com>
2025-06-17 12:33:46 +0100

    Added empty line

--- settings.php
+++ settings.php
@@ -914,6 +914,7 @@ function siteorigin_settings_set( $setting, $value ) {
  * Display's breadcrumbs supported by Breadcrumb NavXT, Rank Math, and Yoast SEO.
  */
 function siteorigin_settings_breadcrumbs( $class = null ) {
+
 	if ( ! apply_filters( 'siteorigin_settings_show_breadcrumbs', true ) ) {
 		return;
 	}
